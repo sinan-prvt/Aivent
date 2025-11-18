b4fa257126887be5044ecd5f467387b108e1f3ef
sinan-prvt <mohamedsinan9400@gmail.com>
2025-11-18 11:48:29 +0530

    updated-for-checking

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from apps.users.views import (
     RegisterView,
-    CustomLoginView,
     SendOTPView,
     VerifyOTPView,
+    TokenObtainPairView,
     SendResetOTPView,
     ResetPasswordView,
 )
@@ -13,7 +13,7 @@ urlpattern = [
     path("send-otp/", SendOTPView.as_view(), name="auth-send-otp"),
     path("verify-otp/", VerifyOTPView.as_view(), name="auth-verify-otp"),
 
-    path("login/", CustomLoginView.as_view(), name="jwt-login"),
+    path("login/", TokenObtainPairView.as_view(), name="jwt-login"),
     path("refresh/", TokenRefreshView.as_view(), name="jwt-refresh"),
     path("send-reset-otp/", SendResetOTPView.as_view(), name="auth-send-reset-otp"),
     path("reset-password/", ResetPasswordView.as_view(), name="auth-reset-password"),
